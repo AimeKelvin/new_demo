78c7066f7ee864355ed2b417d82f8e59e12eb33c
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-07 22:24:10 +0200

    Descent about me done

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,15 +9,18 @@ interface ButtonProps {
 export default function Button({ title, onClick, className = "" }: ButtonProps) {
   return (
     <motion.button
-      className={`py-2 px-6 rounded-lg transition-transform duration-300 ${className}`}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.95 }}
-      initial={{ y: "100%" }} // Start from the top (off-screen)
-      animate={{ y: 0 }}      // Move to the center (its normal position)
-      transition={{ duration: 3 }}
-      onClick={onClick}
-    >
-      {title}
-    </motion.button>
+    className={`py-2 px-6 rounded-lg transition-transform duration-300 border-2 border-black text-black bg-transparent hover:border-gray-800 hover:text-gray-800  mt-4 dark:border-zinc-400 dark:text-zinc-400 ${className}`}
+    whileHover={{ scale: 1.18 }}
+    whileTap={{ scale: 0.95 }}
+    initial={{ y: "100%" }} 
+    animate={{ y: 0 }}      
+    transition={{ duration: 0.7 }}
+    onClick={onClick}
+  >
+    {title}
+  </motion.button>
+  
+
+
   );
 }
--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -1,8 +1,12 @@
 "use client";
 import Particles from "../components/Particles";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import Button from "../components/Button";
 
 export default function About() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-transparent relative">
       <Particles
@@ -10,55 +14,60 @@ export default function About() {
         quantity={100}
       />
       <div className="max-w-4xl text-center space-y-6">
-        {/* Title Animation */}
+        
         <motion.h2
           initial={{ opacity: 0, x: 100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
           viewport={{ once: true }}
-          className="text-4xl font-bold text-zinc-300 hover:text-zinc-100"
+          className="text-5xl font-bold text-zinc-600 py-5 dark:text-zinc-300"
         >
           About Me
         </motion.h2>
 
-        {/* Paragraphs Animation */}
-        <motion.p
+        
+        <motion.div
           initial={{ opacity: 0, x: 100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1.5 }}
           viewport={{ once: true }}
-          className="text-lg text-zinc-500 hover:text-zinc-300 leading-relaxed"
+          className="text-lg text-zinc-500  leading-relaxed relative"
         >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore qui officiis laboriosam fuga, architecto reiciendis autem nostrum mollitia earum necessitatibus similique voluptatibus facere ullam assumenda animi. Amet aliquam in commodi!
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, x: 100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1.8 }}
-          viewport={{ once: true }}
-          className="text-lg text-zinc-500 hover:text-zinc-300 leading-relaxed"
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore qui officiis laboriosam fuga, architecto reiciendis autem nostrum mollitia earum necessitatibus similique voluptatibus facere ullam assumenda animi. Amet aliquam in commodi!
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, x: -100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 2 }}
-          viewport={{ once: true }}
-          className="text-lg text-zinc-500 hover:text-zinc-300 leading-relaxed"
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore qui officiis laboriosam fuga, architecto reiciendis autem nostrum mollitia earum necessitatibus similique voluptatibus facere ullam assumenda animi. Amet aliquam in commodi!
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, x: -100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 2.2 }}
-          viewport={{ once: true }}
-          className="text-lg text-zinc-500 hover:text-zinc-300 leading-relaxed"
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore qui officiis laboriosam fuga, architecto reiciendis autem nostrum mollitia earum necessitatibus similique voluptatibus facere ullam assumenda animi. Amet aliquam in commodi!
-        </motion.p>
+          <div
+            className={`overflow-hidden transition-all duration-500 ease-in-out relative ${
+              expanded ? "max-h-[1000px]" : "max-h-[150px]"
+            }`}
+          >
+            <p>
+              In 2018, I embarked on my journey into software development, initially honing my skills through <strong>Free Code Camp</strong> before delving into a software training bootcamp known as <strong>ATLP</strong>, facilitated by <strong>Andela Rwanda</strong>.
+            </p>
+            <p>
+              Fast forward to today, I boast over 5 years of immersive experience as a Web & Mobile Engineer. Throughout my career, I've collaborated with various engineering firms, contributing my expertise to diverse projects. Checkout my experience section.
+            </p>
+            <p>
+              Among my notable achievements is the creation of <strong>Sinc</strong>, an innovative event ticketing platform, in collaboration with my Co-Founders. Sinc has significantly transformed and enhanced the events industry landscape in Rwanda.
+            </p>
+            <p>
+              Beyond coding, I am passionate about expanding my knowledge in Cyber Security. During my leisure time, you can find me indulging in pool table, socializing with friends, or immersing myself in the vibrant events scene, often facilitated by Sinc.
+            </p>
+          </div>
+        
+          {!expanded && (
+            <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-transparent to-transparent pointer-events-none"></div>
+          )}
+
+          
+        <Button 
+        onClick={() => setExpanded(!expanded)}
+        title={expanded ? "Read Less" : "Read More"}
+        className="light:text-black light:border-black"
+        />
+        </motion.div>
+
       </div>
     </div>
   );
 }
+
+
+
